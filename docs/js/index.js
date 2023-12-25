fde765632e00149725f4a33b24c0cc4efcc124d7
take44444 <take44444.general@gmail.com>
2023-12-25 19:48:44 +0900

    improve front design

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,17 +3,34 @@ import init, { GameBoyHandle, AudioHandle } from "../wasm/gbemu_web.js"
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const rom_button = document.getElementById("rom_button");
+const sav_button = document.getElementById("sav_button");
 const rom_input = document.getElementById("rom_input");
 const sav_input = document.getElementById("sav_input");
-const on = document.getElementById("on");
-const off = document.getElementById("off");
+const power = document.getElementById("power");
 const save = document.getElementById("save");
 
 ctx.fillStyle = "black";
 ctx.fillRect(0.0, 0.0, canvas.width, canvas.height);
 
+function resize_canvas() {
+  let width, height;
+  if ((document.documentElement.clientWidth-250)*144 > (document.documentElement.clientHeight-230)*166) {
+    height = document.documentElement.clientHeight - 230;
+    width = height * 160/144;
+  } else {
+    width = document.documentElement.clientWidth - 250;
+    height = width * 144/160;
+  }
+  canvas.setAttribute('width', width);
+  canvas.setAttribute('height', height);
+  ctx.fillRect(0.0, 0.0, canvas.width, canvas.height);
+}
+
 async function main() {
   await init();
+
+  resize_canvas();
 
   let rom = null;
   let sav = new Uint8Array();
@@ -26,6 +43,7 @@ async function main() {
     reader1.onloadend = (_) => {
       rom = new Uint8Array(reader1.result);
     };
+    rom_button.classList.add('specified');
   };
   sav_input.oninput = (_) => {
     let reader2 = new FileReader();
@@ -33,10 +51,20 @@ async function main() {
     reader2.onloadend = (_) => {
       sav = new Uint8Array(reader2.result);
     }
+    sav_button.classList.add('specified');
   }
 
-  on.onclick = (_) => {
+  window.onresize = resize_canvas
+
+  power.onclick = (_) => {
+    if (running) {
+      power.classList.remove('on');
+      running = false;
+      ctx.fillRect(0.0, 0.0, canvas.width, canvas.height);
+      return;
+    }
     if (rom === null || gameboy !== null) return;
+    power.classList.add('on');
     running = true;
 
     gameboy = GameBoyHandle.new(rom, sav);
@@ -59,8 +87,8 @@ async function main() {
         let image_data = new ImageData(framebuffer, 160, 144);
         createImageBitmap(image_data, {
           resizeQuality: "pixelated",
-          resizeWidth: 640,
-          resizeHeight: 576,
+          resizeWidth: canvas.width,
+          resizeHeight: canvas.height,
         }).then((bitmap) => {
           ctx.drawImage(bitmap, 0.0, 0.0);
         });
@@ -86,18 +114,17 @@ async function main() {
     window.URL.revokeObjectURL(url);
   };
 
-  off.onclick = (_) => {
-    running = false;
-    ctx.fillRect(0.0, 0.0, canvas.width, canvas.height);
+  document.onkeydown = (e) => {
+    if (gameboy !== null) {
+      gameboy.key_down(e.code);
+    }
   };
 
-  document.onkeydown = (e) => {
-    if (gameboy !== null) gameboy.key_down(e.code);
-  }
-
   document.onkeyup = (e) => {
-    if (gameboy !== null) gameboy.key_up(e.code);
-  }
+    if (gameboy !== null) {
+      gameboy.key_up(e.code);
+    }
+  };
 }
 
 main()
